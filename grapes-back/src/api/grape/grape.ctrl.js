c4d6bdf52fa73faa5c76585faf3f6a4042415fdb
SimEunJu <wndmstla123@gmail.com>
2019-04-10 23:08:59 +0900

    순서대로 적용!

--- grapes-back/src/api/grape/grape.ctrl.js
+++ grapes-back/src/api/grape/grape.ctrl.js
@@ -52,22 +52,45 @@ exports.updateTitle = async (ctx) => {
 
 exports.updateOneGrape = async (ctx) => {
    const {gno, idx} = ctx.params;
+   const {title, content} = ctx.request.body;
    try{
-        const grapes = await Grapes.findOneAndUpdate({gno, grape: {idx}}, {grape: ctx.request.body}, {new:true}).exec();
+        await Grapes.updateOne({_id: gno, 'grape.idx': idx}, {$set: {'grape.$.title': title, 'grape.$.content': content}}).exec();
+        const grapes = await Grapes.findById(gno).exec();
         ctx.body = grapes;
     }catch(e){
         ctx.throw(e);
    }
 }
+exports.updateOneGrapeColor = async (ctx) => {
+    const {gno, idx} = ctx.params;
+    const {isChecked} = ctx.request.body;
+    try{
+        await Grapes.updateOne({_id: gno, 'grape.idx': idx}, {$set: {'grape.$.isChecked': isChecked}}).exec();
+        const grapes = await Grapes.findById(gno).exec();
+        ctx.body = grapes;
+     }catch(e){
+         ctx.throw(e);
+    }
+ }
 exports.updateRgba = async (ctx) => {
    const {gno} = ctx.params;
    try{
         const grapes = await Grapes.findByIdAndUpdate(gno, ctx.request.body,{new: true}).exec();
         ctx.body = grapes;
    }catch(e){
+       console.log(e);
        ctx.throw(e);
    }
 }
+exports.updateFinishState = async (ctx) => {
+    const {gno} = ctx.params;
+    try{
+         const grapes = await Grapes.findByIdAndUpdate(gno, ctx.request.body,{new: true}).exec();
+         ctx.body = grapes;
+    }catch(e){
+        ctx.throw(e);
+    }
+ }
 exports.readShowcase = async (ctx) => {
     try{
         const allGrapes = await Grapes.find().exec();
--- grapes-back/src/api/grape/index.js
+++ grapes-back/src/api/grape/index.js
@@ -4,13 +4,14 @@ const grapeCtrl = require("./grape.ctrl");
 const grape = new Router();
 
 grape.get("/:user/new", grapeCtrl.createNew);
+grape.get("/:user/showcase", grapeCtrl.readShowcase);
 
 grape.get("/:gno", grapeCtrl.checkObjectId, grapeCtrl.readGrapes);
-grape.post("/:gno/title", grapeCtrl.checkObjectId, grapeCtrl.updateTitle);
-grape.post("/:gno/:idx", grapeCtrl.checkObjectId, grapeCtrl.updateOneGrape);
-grape.post("/:gno/rgba", grapeCtrl.checkObjectId, grapeCtrl.updateRgba);
-
-grape.get("/:user/showcase", grapeCtrl.readShowcase);
 grape.delete("/:gno", grapeCtrl.checkObjectId, grapeCtrl.deleteGrapes);
+grape.post("/:gno/rgba", grapeCtrl.checkObjectId, grapeCtrl.updateRgba);
+grape.post("/:gno/title", grapeCtrl.checkObjectId, grapeCtrl.updateTitle);
+grape.post("/:gno/finish", grapeCtrl.checkObjectId, grapeCtrl.updateFinishState);
+grape.post("/:gno/:idx", grapeCtrl.checkObjectId, grapeCtrl.updateOneGrape);
+grape.post("/:gno/:idx/color", grapeCtrl.checkObjectId, grapeCtrl.updateOneGrapeColor);
 
 module.exports = grape;
